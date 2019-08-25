d79a5c64acc9de7910c95b07734d8a7503a642e0
Tim Biermann <tbier@posteo.de>
2019-08-26 01:57:07 +0200

    some changes

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-for i in *aux *bbl *bcf *blg *dvi *lof *log *lot *out *run.xml *synctex.gz *tex.latexmain *toc; do
+for i in *aux *bbl *bcf *blg *dvi *lof *log *lot *out *run.xml *synctex.gz \
+  *tex.latexmain *toc *fdb_latexmk *fls *idx *ind *ilg *glg *glo *xdy *gls; do
   rm $i;
 done
