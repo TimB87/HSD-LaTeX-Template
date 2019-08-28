2976cc09687de9acd0e47b6bb82fd82295cc660b
Tim Biermann <tbier@posteo.de>
2019-08-29 01:34:48 +0200

    sync

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 for i in *aux *bbl *bcf *blg *dvi *lof *log *lot *out *run.xml *synctex.gz \
-  *tex.latexmain *toc *fdb_latexmk *fls *idx *ind *ilg *glg *glo *xdy *gls; do
+  *tex.latexmain *toc *fdb_latexmk *fls *idx *ind *ilg *glg *glo *xdy *gls \
+  *nlo *nls; do
   rm $i;
 done
