a54008e2c4e3da248de80f9f71860c8f5834791e
Tim Biermann <tbier@posteo.de>
2019-09-03 05:02:10 +0200

    next round of stuff

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in *aux *bbl *bcf *blg *dvi *lof *log *lot *out *run.xml *synctex.gz \
   *tex.latexmain *toc *fdb_latexmk *fls *idx *ind *ilg *glg *glo *xdy *gls \
-  *nlo *nls; do
+  *nlo *nls *acn *acr *alg *glsdefs arara.xml; do
   rm $i;
 done
