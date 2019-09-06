6a7fdae0385eb98ce12dc82ae20dcc0919801c24
Tim Biermann <tbier@posteo.de>
2019-09-06 13:23:05 +0200

    some more stuff for our appendix

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in *aux *bbl *bcf *blg *dvi *lof *log *lot *out *run.xml *synctex.gz \
   *tex.latexmain *toc *fdb_latexmk *fls *idx *ind *ilg *glg *glo *xdy *gls \
-  *nlo *nls *acn *acr *alg *glsdefs arara.xml *lol; do
+  *nlo *nls *acn *acr *alg *glsdefs arara.xml *lol *toc1; do
   if [ -e $i ]; then rm $i >/dev/null ; fi
 done
